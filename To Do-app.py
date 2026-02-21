b1cc7315d1f48c2fc82653fb2d5deccd78275d9d
Jalkiran <Jalkirankaur36@gmail.com>
2026-02-21 19:10:06 +0530

    Marking task complete

--- To Do-app.py
+++ To Do-app.py
@@ -1,62 +1,74 @@
-tasks=[]
-#Load Tasks when file starts
+tasks = []
+
+# Load tasks
 try:
-    f=open("tasks.txt","r")
-    tasks=f.read().split("\n")
-    if tasks==[" "]:
-        tasks=[]
-    f.close()
+    with open("tasks.txt", "r",encoding="utf-8") as f:
+        tasks = f.read().split("\n")
+        if tasks == [""]:
+            tasks = []
 except:
-    tasks=[]
+    tasks = []
 
 while True:
     print("\n---TO DO LIST---")
     print("1.Add Tasks")
     print("2.View Tasks")
     print("3.Delete Tasks")
-    print("4.Exit")
-    
-    #ADD TASKS
-    choice=input("Enter Choice:")
-    if choice=="1":
-       task=input("Enter new task:")
-       tasks.append(task)
+    print("4.Mark task complete")
+    print("5.Exit")
 
-       #SAVE
-       f=open("tasks.txt","w")
-       f.write("\n".join(tasks))
-       f.close()
+    choice = input("Enter Choice: ")
 
-       print("Task Added!")
+    # ADD
+    if choice == "1":
+        task = input("Enter new task: ")
+        tasks.append(task)
 
-    #VIEW TASKS
-    elif choice=="2":
-       if len(tasks)==0:
-          print("No task yet!")
-       else:    
-         for i , t in enumerate(tasks, start=1):
+        with open("tasks.txt","w",encoding="utf-8") as f:
+            f.write("\n".join(tasks))
+
+        print("Task Added!")
+
+    # VIEW
+    elif choice == "2":
+        if not tasks:
+            print("No task yet!")
+        else:
+            for i, t in enumerate(tasks, start=1):
                 print(i, ".", t)
-    
-    #DELETE TASKS 
+
+    # DELETE
     elif choice == "3":
         num = int(input("Enter task number to delete: "))
         if 1 <= num <= len(tasks):
             removed = tasks.pop(num-1)
 
-            f=open("tasks.txt","w")
-            f.write("\n".join(tasks))
-            f.close()
+            with open("tasks.txt", "w",encoding="utf-8") as f:
+                f.write("\n".join(tasks))
 
             print(removed, "deleted!")
         else:
             print("Invalid number")
 
-    # EXIT
+    # MARK COMPLETE
     elif choice == "4":
+        num = int(input("Enter task to be marked as done: "))
+        if 1 <= num <= len(tasks):
+
+            if not tasks[num-1].startswith("✅"):
+                tasks[num-1] = "✅ " + tasks[num-1]
+
+            with open("tasks.txt", "w",encoding="utf-8") as f:
+                f.write("\n".join(tasks))
+
+            print("Task Marked Complete")
+        else:
+            print("Invalid task")
+
+    # EXIT
+    elif choice == "5":
         print("Goodbye 👋")
         break
 
     else:
         print("Invalid choice")
-
-print("Hehe Guys(winks)")
