d0d651c7eddb75fa8eeb05c1c0bc04dd87b64eee
Jalkiran <Jalkirankaur36@gmail.com>
2026-02-26 22:08:07 +0530

    Abstracted

--- Test.py
+++ Test.py
@@ -6,6 +6,23 @@ def calculator(fields,title):
     root=tk.Tk()
     root.title(title)
     root.geometry("400x500")
+#-------to get numbers------
+    def get_numbers():
+       for field in entries:
+          value=entries[field].get()
+          if value.strip()=="":
+             status_label.config(text="All fields must e filled",fg="red")
+             return None
+       try:
+             num1=float(entries["num1"].get())
+             num2=float(entries["num2"].get())
+             return num1,num2
+       except ValueError:
+             status_label.config(text="Enter valid numbers",fg="red")
+             return None
+       
+       status_label.config(text="",fg="red")
+       return num1,num2
 #--------Field-------
     for field in fields:
         label=tk.Label(root,text=field)
@@ -18,41 +35,42 @@ def calculator(fields,title):
     status_label.pack(pady=5)
 #-------ADD-----------
     def add():
-      num1=float(entries["num1"].get())
-      num2=float(entries["num2"].get())
-      result=num1+num2
-      result_box.insert(tk.END,result)
+      nums=get_numbers()
+      if not nums:
+         return
+      num1,num2=nums
+      result_box.insert(tk.END,num1+num2)
       
     add_btn=tk.Button(root,text="ADD",command=add)
     add_btn.pack(pady=5)
 #---------SUBTRACT-------
     def sub():
-      num1=float(entries["num1"].get())
-      num2=float(entries["num2"].get())
-      if num1>num2:
-        result=num1-num2
-      else :
-         result=num2-num1
-      result_box.insert(tk.END,result)
+      nums=get_numbers()
+      if not nums:
+         return
+      num1,num2=nums
+      result_box.insert(tk.END,num1-num2)
     sub_btn=tk.Button(root,text="SUBTRACT",command=sub)
     sub_btn.pack(pady=5)   
 #--------MULTIPLY------
     def mul():
-      num1=float(entries["num1"].get())
-      num2=float(entries["num2"].get())
-      result=num1*num2
-      result_box.insert(tk.END,result)
+      nums=get_numbers()
+      if not nums:
+         return
+      num1,num2=nums
+      result_box.insert(tk.END,num1*num2)
     mul_btn=tk.Button(root,text="MULTIPLY",command=mul)
     mul_btn.pack(pady=5)  
 #--------DIVIDE--------
     def div():
-      num1=float(entries["num1"].get())
-      num2=float(entries["num2"].get())
+      nums=get_numbers()
+      if not nums:
+         return
+      num1,num2=nums
       if num2==0:
          status_label.config(text="Not divisible by zero",fg="red")
       else:
-         result=num1/num2
-         result_box.insert(tk.END,result)
+         result_box.insert(tk.END,num1/num2)
     div_btn=tk.Button(root,text="DIVIDE",command=div)
     div_btn.pack(pady=5)  
       
@@ -61,10 +79,11 @@ def calculator(fields,title):
      data={}
      for field in entries:
         value=entries[field].get()
-        if value.strip()=="":
+        '''if value.strip()=="":
             status_label.config(text="All fields mustt be filled",fg="red")
-            return
+            return'''
         data[field]=value
+     '''data[field]=entry.get()'''
      print(data)    
      status_label.config(text="Submitted",fg="green")
      for field in entries:
